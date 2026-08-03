7101ec4a799305366a474d093a33d051e55622c1
vedloya11 <vedloya@gmail.com>
2026-08-03 19:41:33 +0530

    Time: 30 ms (91.39%), Space: 48.6 MB (50.82%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer>set=new HashSet<>();
+        int n = candyType.length ;
+        for(int c : candyType){
+            set.add(c);
+        }
+        if(set.size() > n/2){
+            return n/2;
+        }else{
+            return set.size();
+        }
+
+        
+    }
+}
