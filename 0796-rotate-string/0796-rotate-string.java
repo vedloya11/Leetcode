24595bab3fecb24c571461f21f437c1218bf28e6
vedloya11 <vedloya@gmail.com>
2026-08-28 00:26:12 +0530

    Time: 2 ms (64.97%), Space: 43.4 MB (67.91%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        String ans = s +s;
+        if(ans.contains(goal)){
+            return true;
+        }
+        return false;
+        
+    }
+}
