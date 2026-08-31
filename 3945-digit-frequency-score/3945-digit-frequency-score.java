8b46784e4f997b6938d3ae05c3d867fdeb9d2468
vedloya11 <vedloya@gmail.com>
2026-08-31 18:44:07 +0530

    Time: 1 ms (98.36%), Space: 42.6 MB (55.77%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int ans = 0;
+        while (n > 0) {
+            ans += n % 10;
+            n /= 10;
+        }
+        return ans;
+
+    }
+}
